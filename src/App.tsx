5be864fa8dc35dd168833a330b05d127ac2d4607
uzresk <uzr.esk@gmail.com>
2022-02-13 10:02:29 +0900

    再生秒数を取得して5秒毎に再生時間を表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {HTMLAttributes, useEffect, useMemo, useRef, VideoHTMLAttributes} from 'react';
+import React, {SyntheticEvent, useEffect, useMemo, useRef, useState} from 'react';
 import './App.css';
 import Hls from "hls.js"
 
@@ -8,6 +8,24 @@ const App = () => {
 
   const isSupportedBrowser = useMemo(() => Hls.isSupported(), [])
   const ref = useRef<HTMLVideoElement>(null)
+
+  const [timeToWatch, setTimeToWatch] = useState(0)
+
+  const play = () => {
+    console.log("play start")
+  }
+
+  // 5秒毎に記録する
+  const timeupdate = (event: SyntheticEvent<HTMLVideoElement>) => {
+    const currentTime: number = Math.floor(event.currentTarget.currentTime);
+    const i = currentTime % 5
+    if (i === 0 && currentTime !== timeToWatch) {
+      console.log(currentTime)
+      // ここでログ記録
+      setTimeToWatch(currentTime)
+    }
+  }
+
   useEffect(() => {
     if (isSupportedBrowser && ref.current != null) {
       const hls = new Hls()
@@ -23,7 +41,10 @@ const App = () => {
     <div className="App">
       <div className="content">
         {isSupportedBrowser ? (
-          <video ref={ref} width="850" controls/>
+          <>
+          <video ref={ref} width="850" controls onPlay={play} onTimeUpdate={timeupdate}/>
+          <div>{timeToWatch}</div>
+          </>
         ) : (
           <div>
             not support browser
